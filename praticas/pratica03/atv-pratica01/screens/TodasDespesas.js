9eac6dfb30a7b5b7fc0196fb44ab49fbf60e78df
João Victor <joaovic.scruz@gmail.com>
2026-04-10 00:12:31 -0300

    Feat: Implementa navegação, telas de despesas e formulário de gerenciamento

--- praticas/pratica03/atv-pratica01/screens/TodasDespesas.js
+++ praticas/pratica03/atv-pratica01/screens/TodasDespesas.js
@@ -1,9 +1,25 @@
-import {Text} from 'react native'
+import DespesaSaida from '../components/despesa/DespesaSaida';
 
 function TodasDespesas() {
-    return (
-        <Text>TodasDespesas</Text>
-    )
 
+  const DUMMY_DESPESAS = [
+    {
+      id: '1',
+      descricao: 'Conta de luz',
+      valor: 100.99,
+      data: new Date(2025, 2, 11)
+    },
+    {
+      id: '2',
+      descricao: 'Conta de Agua',
+      valor: 40.99,
+      data: new Date(2025, 4, 10)
+    }
+  ]
+
+  return (
+    <DespesaSaida despesas={DUMMY_DESPESAS} periodo={'Total'} />
+  )
 }
-export default TodasDespesas
+
+export default TodasDespesas;
